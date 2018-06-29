9a50dd41b07c131c0d3ee0594d8b288eac9349d3
elamahpla <theamazingness@gmail.com>
2018-06-28 20:02:36 -0500

    Whoops, messed something up XD

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+  .then(() => console.log('Nice, you connected'))
+  .catch((error) => console.log(error));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
