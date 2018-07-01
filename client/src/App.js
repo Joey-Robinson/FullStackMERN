572b3ba69e79bc8e72989474d46d32f2cec8cfc2
elamahpla <theamazingness@gmail.com>
2018-07-01 13:12:09 -0500

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import AppNavBar from './Components/NavBar/AppNavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <AppNavBar />
       </div>
     );
   }
