75949964ddc04027507985de856966317084d895
elamahpla <theamazingness@gmail.com>
2018-07-01 14:30:04 -0500

    More

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import AppNavBar from './Components/NavBar/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavBar />
+        <ShoppingList />
       </div>
     );
   }
